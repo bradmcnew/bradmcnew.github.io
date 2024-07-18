44f34ffaf33dd3b226b611f75edb0aecfac205d3
Brad McNew <mcnewbrad@gmail.com>
2024-07-17 23:42:16 -0400

    mobile accessibility

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,46 +1,101 @@
-import { Link, NavLink } from 'react-router-dom';
-import './index.scss';
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faEnvelope,
+  faHome,
+  faSuitcase,
+  faUser,
+  faBars,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
-const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo'>
-            <img src={ LogoS } alt="logo" />
-            <img className="sub-logo" src={ LogoSubtitle } alt="sublogo" />
-        </Link>
-        <nav>
-            <NavLink exact="true" activclassname="active" to="/">
-                <FontAwesomeIcon icon={ faHome } color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={ faUser } color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activclassname="active" className="portfolio-link" to="/portfolio">
-                <FontAwesomeIcon icon={ faSuitcase } color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={ faEnvelope } color="#4d4d4e" />
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/bradmcnew/'
-                >
-                    <FontAwesomeIcon icon={ faLinkedin } color='#4d4d4e' />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://github.com/bradmcnew'
-                >
-                    <FontAwesomeIcon icon={ faGithub } color='#4d4d4e' />
-                </a>
-            </li>
-        </ul>
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
+
+  return (
+    <div className="nav-bar">
+      <Link className="logo">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="sublogo" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink
+          exact="true"
+          activclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/bradmcnew/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/bradmcnew"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
     </div>
-)
+  )
+}
 
-export default Sidebar;
+export default Sidebar
