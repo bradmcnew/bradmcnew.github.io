0503510564335f87a553500fd4b9cef79ae2ee2e
Brad McNew <mcnewbrad@gmail.com>
2024-07-30 21:33:39 -0400

    logo image in homepage logo

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,13 +1,13 @@
 import { useEffect, useRef } from 'react'
 import './index.scss'
 import anime from 'animejs'
+import logoImage from '../../../assets/images/logo-s.png'
 
 const Logo = () => {
   const logoRef = useRef(null)
   useEffect(() => {
     const anim = anime.timeline({
-      loop: true,
-      direction: 'alternate',
+      loop: false,
     })
 
     anim
@@ -16,16 +16,15 @@ const Logo = () => {
         strokeDashoffset: [anime.setDashoffset, 0],
         easing: 'easeInOutQuart',
         duration: 2000,
-        delay: function (el, i) {
-          return i * 250
-        },
+        delay: anime.stagger(250, { start: 1000 }),
       })
       .add({
-        targets: '#hexagon #B',
+        targets: '#logo-image',
         duration: 1000,
         opacity: 1,
         easing: 'easeInOutQuart',
       })
+
     // Clean up animation when component unmounts
     return () => {
       anim.pause()
@@ -39,31 +38,23 @@ const Logo = () => {
         viewBox="0 0 100 100"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink" // Corrected xlink attribute
+        xmlnsXlink="http://www.w3.org/1999/xlink"
       >
         <g>
-          <g
-            id="B"
-            transform="translate(36, 33)"
-            fill="#ffd700"
-            style={{ opacity: 0 }}
-          >
-            <text
-              x="0.141666985"
-              y="33"
-              fontFamily="system-ui, Calibre-Medium, Calibre, sans-serif"
-              fontSize="50"
-              fontWeight="400"
-              letterSpacing="4.16666603"
-            >
-              B
-            </text>
-          </g>
+          <image
+            id="logo-image"
+            x="20"
+            y="20"
+            width="60"
+            height="60"
+            xlinkHref={logoImage}
+            opacity="0"
+          />
           <path
             stroke="#ffd700"
-            strokeWidth="5" // Corrected stroke-width attribute
-            strokeLinecap="round" // Corrected stroke-linecap attribute
-            strokeLinejoin="round" // Corrected stroke-linejoin attribute
+            strokeWidth="5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M 50, 5 L 11, 27 L 11, 72 L 50, 95 L 89, 73 L 89, 28 z"
           />
         </g>
